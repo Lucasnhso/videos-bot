31809e12cdaabbc029f77c102aa173a03861b934
Lucas Oliveira <lucas.henrique@minu.co>
2024-06-27 16:18:47 -0300

    feat: add retry on traslation from gpt

--- src/providers/gpt/client.js
+++ src/providers/gpt/client.js
@@ -6,14 +6,30 @@ class GptClient {
   }
   #client;
 
-  async translate({ text, source, target}) {
-    const { translation } = await this.#client.translation({
-      text,
-      source,
-      target
-    })
+  async translate(text, { source = 'en', target = 'pt' } = {}) {
+    return this.#retry(async () => {
+      const { translation } = await this.#client.translation({
+        text,
+        source,
+        target
+      });
+      return translation.result;
+    }, 3, 200);
+  }
 
-    return translation.result
+  async #retry(fn, retries, delay) {
+    for (let i = 0; i <= retries; i++) {
+      try {
+        return await fn();
+      } catch (error) {
+        if (i === retries) throw error;
+        await this.#delay(delay);
+      }
+    }
+  }
+
+  #delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 }
 
