37fe10e8c5127afcff35589dc199b4430a38d7cc
Lucas Oliveira <lucas.henrique@minu.co>
2024-07-01 11:43:48 -0300

    feat: add publishAt on create Post

--- src/modules/posts/services/create.js
+++ src/modules/posts/services/create.js
@@ -1,10 +1,11 @@
 const path = require('node:path');
+const fs = require('node:fs');
 
 const postsRepository = require('../repository');
 const clipsRepository = require("../../clips/repository");
 const YoutubeClient = require("../../../providers/youtube/client");
 
-async function create(platforms) {
+async function create(platforms, publishAt) {
   for(const platform of platforms) {
     const clip = await clipsRepository.findOneToPost(platform);
     const clipPath = path.join(__dirname, '../../../../data/pending', `${clip.id}.mp4`);
@@ -16,14 +17,15 @@ async function create(platforms) {
         path: clipPath,
         title: clip.title,
         description: clip.description,
-        tags: ['shorts'],
-      });
+        tags: ['shorts']
+      }, { publishAt });
 
     }
     await postsRepository.create({
       platform,
       clipId: clip.id
     })
+    // fs.renameSync(`../../../../data/done/${clip.id}.mp4`)
   }
 }
 
